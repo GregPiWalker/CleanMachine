e0a6ff9c1345b72c9ac167b4922a7c477847e14b
Greg Walker <imalsoamember@yahoo.com>
2020-02-21 19:12:50 -0700

    Add initial Unit Test project

---
+++ CleanMachine.Tests/TestStateMachine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClewanMachine.Tests
+{
+    [TestClass]
+    public class TestStateMachine
+    {
+        [TestInitialize]
+        public void Init()
+        { }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
