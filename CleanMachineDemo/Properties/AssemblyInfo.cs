8137d2f77368de66fef12efb2ede559da876cf28
Greg Walker <imalsoamember@yahoo.com>
2020-02-23 14:24:34 -0700

    Add unit test project to demo solution

--- CleanMachineDemo/Properties/AssemblyInfo.cs
+++ CleanMachineDemo/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VisualDemo")]
+[assembly: AssemblyTitle("CleanMachineDemo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VisualDemo")]
+[assembly: AssemblyProduct("CleanMachineDemo")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
